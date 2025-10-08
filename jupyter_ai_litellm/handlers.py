953e3cffa1268e771df20f1f6c0af97ce00efad8
Piyush Jain <piyushjain@duck.com>
2025-10-08 13:44:22 -0700

    Added litellm, models handler

--- jupyter_ai_litellm/handlers.py
+++ jupyter_ai_litellm/handlers.py
@@ -4,6 +4,8 @@ from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
 import tornado
 
+from .chat_models_rest_api import ChatModelsRestAPI
+
 class RouteHandler(APIHandler):
     # The following decorator should be present on all verb methods (head, get, post,
     # patch, put, delete, options) to ensure only authorized user can request the
@@ -19,6 +21,10 @@ def setup_handlers(web_app):
     host_pattern = ".*$"
 
     base_url = web_app.settings["base_url"]
+    print(f"Base url is {base_url}")
     route_pattern = url_path_join(base_url, "jupyter-ai-litellm", "get-example")
-    handlers = [(route_pattern, RouteHandler)]
+    handlers = [
+        (route_pattern, RouteHandler),
+        (url_path_join(base_url, "api/models/chat") + r"(?:\?.*)?", ChatModelsRestAPI)
+    ]
     web_app.add_handlers(host_pattern, handlers)
